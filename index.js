02d04d3808dc89db8086e942f379d8ef90c6b85a
Manjeet-singh-thakur <manjeetdhar@gmail.com>
2023-11-07 17:38:34 +0530

    dom manipulation

---
+++ index.js
@@ -0,0 +1,19 @@
+// let a = document.querySelector('.most')
+
+// a = document.querySelector('#img');
+
+// a  =document.querySelectorAll('.most');
+// a = document.getElementById('img');
+// a = document.getElementsByClassName('most');
+// a = document.getElementsByTagName('div');
+// a = document.createElement('h3');
+// a.textContent = 'hello world'
+// a.innerHTML = '<button class = "pop">click here</button>'
+// a.classList.add('new','old');
+// a = document.getElementById('check')
+// a.append = 'manjeet';
+
+let  b = document.querySelector('#check')
+b.remove();
+// a.remove('check');
+console.log(b);
